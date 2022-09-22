79840015844672af249c0d58ab81bef3a1766547
Michał Murawski <homasha@gmail.com>
2022-09-22 13:52:35 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from unittest import case
+
+print('1. Założyć nowe konto')
+print('2. Skorzystać z bankomatu')
+print('2. Skorzystać z wpłatomatu')
+number = int(input('Wybierz numer akcji, którą chcesz wykonać: '))
+
+if number == 1:
+    print('1')
+elif number == 2:
+    print('2')
+elif number == 3:
+    print('3')

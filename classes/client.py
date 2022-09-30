b4122b95548fac8ed2890ee693e7126ef10a24a2
Michał Murawski <homasha@gmail.com>
2022-09-30 22:30:44 +0200

    Adds new methods

--- classes/client.py
+++ classes/client.py
@@ -1,3 +1,4 @@
+import random
 import re
 
 
@@ -5,45 +6,114 @@ class Client:
 
     @staticmethod
     def first_name():
-        first_name_input = str(input('Podaj imię: '))
-        first_name = "^[A-Z][a-z]{2}[^0-9]?"
-        index = 0
-        while index == 0:
+        index = 1
+        while index == 1:
+            first_name_input = str(input('Podaj imię: '))
+            first_name = "^[A-Z][a-z]{2,}[^0-9]{1,}?"
             try:
-                if re.search(first_name, first_name_input) is True:
+                if re.search(first_name, first_name_input):
                     return first_name
-                index = 1
             except TypeError:
-                if re.search(first_name, first_name_input) is False:
-                    print('Niepoprawne imię!')
-                index = 0
+                print('Niepoprawne imię!')
+
 
     @staticmethod
     def second_name():
-        second_name = str(input('Podaj nazwisko: '))
-        return second_name
+        index = 1
+        while index == 1:
+            second_name_input = str(input('Podaj nazwisko: '))
+            second_name = "^[A-Z][a-z]{2,}[^0-9]{1,}?"
+            try:
+                if re.search(second_name, second_name_input):
+                    return  second_name
+            except TypeError:
+                print('Niepoprawne nazwisko!')
+
 
     @staticmethod
     def city():
-        city = str(input('Podaj miasto: '))
-        return city
+        index = 1
+        while index == 1:
+            city_input = str(input('Podaj miasto: '))
+            city = "^[A-Z][a-z]{2,}|[A-Z][a-z]{2,}([ ]{1}|[-]{1})[A-Z][a-z]?"
+            try:
+                if re.search(city, city_input):
+                    return city
+            except TypeError:
+                print('Niepoprawna nazwa miasta!')
+
 
     @staticmethod
     def post_code():
-        post_code = str(input('Podaj kod pocztowy: '))
-        return post_code
+        index = 1
+        while index == 1:
+            post_code_input = str(input('Podaj kod pocztowy: '))
+            post_code = "^[0-9]{2}[-]{1}[0-9]{3}?"
+            try:
+                if re.search(post_code, post_code_input):
+                    return post_code
+            except TypeError:
+                print("Niepoprawny format kodu pocztowego!")
+
 
     @staticmethod
     def street():
-        street = str(input('Podaj ulicę: '))
-        return street
+        index = 1
+        while index == 1:
+            street_input = str(input('Podaj ulicę: '))
+            street = "^[A-Z][a-z]{2,}|[A-Z][a-z]{2,}[ ][A-Z][a-z]{2,}?"
+            try:
+                if re.search(street, street_input):
+                    return street
+            except TypeError:
+                print('Niepoprawna ulica!')
+
 
     @staticmethod
     def number_home():
-        number_home = str(input('Podaj nr domu: '))
-        return number_home
+        index = 1
+        while index == 1:
+            number_home_input = str(input('Podaj nr domu: '))
+            number_home = "^[0-9]{1,}?"
+            try:
+                if re.search(number_home, number_home_input):
+                    return number_home
+            except TypeError:
+                print('Nr domu musi być cyfrą!')
+
 
     @staticmethod
     def number_apartment():
-        number_apartment = str(input('Podaj nr mieszkania: '))
-        return number_apartment
+        index = 1
+        while index == 1:
+            number_apartment_input = str(input('Podaj nr mieszkania: '))
+            number_apartment = "^[0-9]{1}?"
+            try:
+                if re.search(number_apartment, number_apartment_input):
+                    return number_apartment
+            except TypeError:
+                print('Nr domu musi być cyfrą! (0 jeżeli nie posiada)!')
+
+
+    @staticmethod
+    def creat_number_acount():
+        index = 1
+        print('Twój numer konto: ', end='')
+        while index <= 26:
+            print(random.randint(0, 9), end='')
+            index += 1
+
+
+    @staticmethod
+    def creat_login():
+        index = 1
+        print('Twój login: ', end='')
+        while index <= 4:
+            print(random.randint(0, 9), end='')
+            index += 1
+        print()
+
+
+    @staticmethod
+    def creat_pin():
+        pin = input('Utwórz 4 cyfrowy PIN: ')
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import self as self
 
 import classes.client
@@ -5,21 +7,30 @@ import classes.client
 print('1. Założyć nowe konto')
 print('2. Skorzystać z bankomatu')
 print('2. Skorzystać z wpłatomatu')
+print('4. Wyjść z programu')
 
-
-try:
-    number = int(input('Wybierz numer akcji, którą chcesz wykonać: '))
-    if number == 1:
-        classes.client.Client.first_name()
-        classes.client.Client.second_name()
-        classes.client.Client.city()
-        classes.client.Client.post_code()
-        classes.client.Client.street()
-        classes.client.Client.number_home()
-        classes.client.Client.number_apartment()
-    elif number == 2:
-        print('2')
-    elif number == 3:
-        print('3')
-except ValueError:
-    print('Nr się nie zgadza! Wybierz 1 lub 2 lub 3.')
+index = 1
+while index == 1:
+    try:
+        number = int(input('Wybierz numer akcji, którą chcesz wykonać: '))
+        if number == 1:
+            classes.client.Client.first_name()
+            classes.client.Client.second_name()
+            classes.client.Client.city()
+            classes.client.Client.post_code()
+            classes.client.Client.street()
+            classes.client.Client.number_home()
+            classes.client.Client.number_apartment()
+            classes.client.Client.creat_number_acount()
+            print()
+            classes.client.Client.creat_login()
+            classes.client.Client.creat_pin()
+            print()
+        elif number == 2:
+            print('2')
+        elif number == 3:
+            print('3')
+        elif number == 4:
+            sys.exit()
+    except ValueError:
+        print('Nr się nie zgadza! Wybierz 1 lub 2 lub 3.')

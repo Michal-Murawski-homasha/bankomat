850fe0455183c4699de8176e1623ba549ddd0112
Michał Murawski <homasha@gmail.com>
2022-10-16 10:54:30 +0200

    Adds parameter self

--- classes/client.py
+++ classes/client.py
@@ -6,77 +6,93 @@ from classes import connection
 
 class Client:
 
-    def first_name(self):
+    def __init__(self):
+        self.first_name_input = None
+        self.first_name = None
+        self.second_name_input = None
+        self.second_name = None
+        self.city_input = None
+        self.city = None
+        self.post_code_input = None
+        self.post_code = None
+        self.street_input = None
+        self.street = None
+        self.number_home_input = None
+        self.number_home = None
+        self.number_apartment_input = None
+        self.number_apartment = None
+
+    def first_name(self, first_name_input, first_name):
         index = 1
         while index == 1:
-            first_name_input = str(input('Podaj imię: '))
-            first_name = "^[A-Z][a-z]{2,}[^0-9]{1,}?"
+            self.first_name_input = str(input('Podaj imię: '))
+            self.first_name = "^[A-Z][a-z]{2,}[^0-9]{1,}?"
             try:
                 if re.search(first_name, first_name_input):
-                    return first_name
+                    return first_name_input
             except TypeError:
                 print('Niepoprawne imię!')
 
-    def second_name(self):
+    def second_name(self, second_name_input, second_name):
         index = 1
         while index == 1:
-            second_name_input = str(input('Podaj nazwisko: '))
-            second_name = "^[A-Z][a-z]{2,}[^0-9]{1,}?"
+            self.second_name_input = str(input('Podaj nazwisko: '))
+            self.second_name = "^[A-Z][a-z]{2,}[^0-9]{1,}?"
             try:
                 if re.search(second_name, second_name_input):
                     return second_name
             except TypeError:
                 print('Niepoprawne nazwisko!')
 
-    def city(self):
+    def city(self, city_input, city):
         index = 1
         while index == 1:
-            city_input = str(input('Podaj miasto: '))
-            city = "^[A-Z][a-z]{2,}|[A-Z][a-z]{2,}([ ]{1}|[-]{1})[A-Z][a-z]?"
+            self.city_input = str(input('Podaj miasto: '))
+            self.city = "^[A-Z][a-z]{2,}|[A-Z][a-z]{2,}([ ]{1}|[-]{1})[A-Z][a-z]?"
             try:
                 if re.search(city, city_input):
                     return city
             except TypeError:
                 print('Niepoprawna nazwa miasta!')
 
-    def post_code(self):
+    def post_code(self, post_code_input, post_code):
         index = 1
         while index == 1:
-            post_code_input = str(input('Podaj kod pocztowy: '))
-            post_code = "^[0-9]{2}[-]{1}[0-9]{3}?"
+            self.post_code_input = str(input('Podaj kod pocztowy: '))
+            self.post_code = "^[0-9]{2}[-]{1}[0-9]{3}?"
             try:
                 if re.search(post_code, post_code_input):
                     return post_code
             except TypeError:
                 print("Niepoprawny format kodu pocztowego!")
 
-    def street(self):
+    def street(self, street_input, street):
         index = 1
         while index == 1:
-            street_input = str(input('Podaj ulicę: '))
-            street = "^[A-Z][a-z]{2,}|[A-Z][a-z]{2,}[ ][A-Z][a-z]{2,}?"
+            self.street_input = str(input('Podaj ulicę: '))
+            self.street = "^[A-Z][a-z]{2,}|[A-Z][a-z]{2,}[ ][A-Z][a-z]{2,}?"
             try:
                 if re.search(street, street_input):
                     return street
             except TypeError:
                 print('Niepoprawna ulica!')
 
-    def number_home(self):
+    def number_home(self, number_home_input, number_home):
         index = 1
         while index == 1:
-            number_home_input = str(input('Podaj nr domu: '))
-            number_home = "^[0-9]{1,}?"
+            self.number_home_input = str(input('Podaj nr domu: '))
+            self.number_home = "^[0-9]{1,}?"
             try:
                 if re.search(number_home, number_home_input):
                     return number_home
             except TypeError:
                 print('Nr domu musi być cyfrą!')
 
-    def number_apartment(self):
+    def number_apartment(self, number_apartment_input, number_apartment):
         index = 1
         while index == 1:
-            number_apartment_input = str(input('Podaj nr mieszkania: '))
-            number_apartment = "^[0-9]{1}?"
+            self.number_apartment_input = str(input('Podaj nr mieszkania: '))
+            self.number_apartment = "^[0-9]{1}?"
             try:
                 if re.search(number_apartment, number_apartment_input):
                     return number_apartment
